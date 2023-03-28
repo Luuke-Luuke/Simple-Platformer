a20623d7865407e696fb5b48a893ccd3fa587185
Luuke-Luuke <116306825+Luuke-Luuke@users.noreply.github.com>
2023-03-28 13:53:26 -0500

    We did a thing
    We did a thing. It's been a week so I don't remember

---
+++ Player.gd
@@ -0,0 +1,37 @@
+extends KinematicBody2D
+
+var velocity = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	apply_gravity()
+	var input = Vector2.ZERO
+	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	
+	if input.x == 0:
+		apply_friction()
+	else:
+		apply_acceleration(input.x)
+	
+	# player movement
+	# if Input.is_action_pressed("ui_right"):
+		# velocity.x = 50
+	# elif Input.is_action_pressed("ui_left"):
+		# velocity.x = -50
+	# else:
+		# velocity.x = 0
+
+	# player jump
+	if Input.is_action_just_pressed("ui_up"):
+		velocity.y = -100
+	# apply movement to the player
+	velocity = move_and_slide(velocity)
+
+func apply_gravity():
+	# gravity
+	velocity.y += 4
+
+func apply_friction():
+	velocity.x = move_toward(velocity.x, 0, 20)
+
+func apply_acceleration(amount):
+	velocity.x = move_toward(velocity.x, 50 * amount, 20)
